42c66622867cb4e81ba267bb216300fa314efae3
naoki-mrnk <20cm0135@jec.ac.jp>
2022-07-14 09:17:38 +0900

    Initial Commit

---
+++ SwiftUI-OneShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-OneShot
+//
+//  Created by Naoki Muramoto on 2022/07/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-OneShot/SwiftUI_OneShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OneShotApp.swift
+//  SwiftUI-OneShot
+//
+//  Created by Naoki Muramoto on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OneShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
